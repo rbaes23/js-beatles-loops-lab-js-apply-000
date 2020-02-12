4d05b6ea7e8a4c2e8968ede2203cde6e19cef84b
Rochelle Anne Baes <rochelleannebaes@gmail.com>
2020-02-12 19:33:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(n) {
   var array = [];
-  var i = 0
   do {
     array.push(`I love the Beatles!`)
-  } while ()
+  } while (n < 15);
+  return array;
 }
