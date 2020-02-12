fcfbe2687401fade705838cb8ffc6974a2b8bea4
Rochelle Anne Baes <rochelleannebaes@gmail.com>
2020-02-12 19:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
-  return array;
   }
+  return array;
 }
